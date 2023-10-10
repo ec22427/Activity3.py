3ebd3360c729a1b67b64246a2933e5aac035fe47
ec22427 <ec22427@qmul.ac.uk>
2023-10-10 10:21:24 +0100

    'plot'

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,12 @@
-print ("hello world")
+import matplotlib.pyplot as plt
+import pandas as pd
+
+file = pd.read_csv('linear_data.csv')
+x_axis = file['X']
+y_axis = file['Y']
+
+plt.plot(x_axis, y_axis)
+plt.xlabel("X-Axis")
+plt.ylabel("Y-Axis")
+plt.show()
+
